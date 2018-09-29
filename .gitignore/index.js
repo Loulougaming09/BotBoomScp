481c40f4b7350e53d5ff379853f2cf7da020e9df
Loulougaming09 <40712776+Loulougaming09@users.noreply.github.com>
2018-09-29 10:36:41 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,7 +10,7 @@ const queue = new Map();
 
 var servers = {};
 
-client.login("NDg0MzY3ODU5NTE5MzI0MTkw.DpCwtg.hOCzv5BSGyJ2F7NOW3BcW7EtLwc");
+client.login("process.env.TOKEN");
 
 function play(connection, message) {
   
